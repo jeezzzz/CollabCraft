de354bd3426ba4258faabd87a4c7bb363f402e40
ajeeshrawal <ajeeshrawal3376@gmail.com>
2023-12-09 01:04:22 +0530

    register screens added

--- app/src/main/java/com/jeezzzz/collabcraft/MainActivity.kt
+++ app/src/main/java/com/jeezzzz/collabcraft/MainActivity.kt
@@ -2,36 +2,28 @@ package com.jeezzzz.collabcraft
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignInResult
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.jeezzzz.collabcraft.R
-import com.jeezzzz.collabcraft.SecondActivity
-import com.jeezzzz.collabcraft.databinding.ActivityMainBinding
-//import com.yourpackage.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), OnConnectionFailedListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
@@ -41,59 +33,53 @@ class MainActivity : AppCompatActivity(), OnConnectionFailedListener {
             .requestEmail()
             .build()
 
-        val googleApiClient = GoogleApiClient.Builder(this)
-            .enableAutoManage(this, this)
-            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        binding.googleSigninButton.setOnClickListener {
-            val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
-            startActivityForResult(signInIntent, 1)
+        findViewById<Button>(R.id.google_signin_button).setOnClickListener {
+            signIn()
         }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 1) {
-            val result = data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }
-            if (result != null) {
-                handleSignInResult(result)
-            }
-        }
-    }
-
-    private fun handleSignInResult(result: GoogleSignInResult) {
-        if (result.isSuccess) {
-            val account: GoogleSignInAccount? = result.signInAccount
-            if (account != null) {
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
                 firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Google Sign In failed
+                showToast("Google Sign In failed: ${e.message}")
             }
-        } else {
-            // Handle sign-in failure
-            showToast("Sign-in failed")
         }
     }
 
     private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
         val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
         firebaseAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+            .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success
                     val intent = Intent(this@MainActivity, SecondActivity::class.java)
                     startActivity(intent)
                     finish()
                 } else {
-                    // If sign in fails, display a message to the user.
-                    showToast("Authentication failed")
+                    // If sign in fails, display a message to the user with the error details
+                    val error = task.exception
+                    if (error != null) {
+                        showToast("Authentication failed: ${error.message}")
+                    } else {
+                        showToast("Authentication failed for an unknown reason")
+                    }
                 }
-            })
-    }
-
-    override fun onConnectionFailed(connectionResult: ConnectionResult) {
-        // Handle connection failures
-        showToast("Connection failed")
+            }
     }
 
     private fun showToast(message: String) {
---
+++ app/src/main/java/com/jeezzzz/collabcraft/RegisterScreen.kt
@@ -0,0 +1,11 @@
+package com.jeezzzz.collabcraft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen)
+    }
+}
