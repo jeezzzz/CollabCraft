012f4563ac6df042f59fe57a2d3089f38932b6fb
ajeeshrawal <ajeeshrawal3376@gmail.com>
2024-01-16 23:48:05 +0530

    register screens changes made

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation( "androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
